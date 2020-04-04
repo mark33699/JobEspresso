ffcc365f5af9fffeb85aec436f609f433c7e5b42
mark <mark.appledeveloper@gmail.com>
2020-04-05 03:45:33 +0800

    create viewModel

---
+++ JobEspresso/PharmaciesViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  PharmaciesViewModel.swift
+//  JobEspresso
+//
+//  Created by 謝飛飛 on 2020/4/5.
+//  Copyright © 2020 MarkFly. All rights reserved.
+//
+
+import Foundation
+
+class PharmaciesViewModel
+{
+    var pharmacies: Dictionary<String,Int> = [:]
+    var countrys: Array<String> = []
+    var completionHandler: (() -> Void) = {}
+
+    func fetchPharmacies()
+    {
+        
+    }
+
+    func sumMaskByCountry()
+    {
+        
+    }
+}
