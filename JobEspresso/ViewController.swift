3daa7f22fd9aeb165f00cff6b800a5b24e68d0ef
mark <mark.appledeveloper@gmail.com>
2020-04-05 01:25:51 +0800

    Initial Commit

---
+++ JobEspresso/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JobEspresso
+//
+//  Created by 謝飛飛 on 2020/4/5.
+//  Copyright © 2020 MarkFly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ JobEspressoTests/JobEspressoTests.swift
@@ -0,0 +1,34 @@
+//
+//  JobEspressoTests.swift
+//  JobEspressoTests
+//
+//  Created by 謝飛飛 on 2020/4/5.
+//  Copyright © 2020 MarkFly. All rights reserved.
+//
+
+import XCTest
+@testable import JobEspresso
+
+class JobEspressoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
